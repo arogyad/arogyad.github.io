4c90a3ab189f51207206c75a0c740f408565d187
arogyad <arogyadahal20@gmail.com>
2021-10-01 21:56:18 +0545

    Margin Fix

--- src/lib/SideBar.svelte
+++ src/lib/SideBar.svelte
@@ -6,7 +6,7 @@
 	id="sidebar"
 	class="md:h-full md:w-60 md:fixed bg-green-900 dark:bg-black text-white duration-300 text-center text-sm"
 >
-	<div id="items" class="py-4 md:pt-40 grid text-center">
+	<div id="items" class="md:pt-40 grid text-center">
 		<a href="/" class="font-bold text-6xl font-serif hover:underline text-white">Arogya Dahal</a>
 		<span class="font-extralight p-5 text-white">Welcome to my blog!</span>
 		<a href="/" class="font-normal p-1 text-xl text-white hover:text-2xl duration-300">Home</a>
@@ -16,7 +16,7 @@
 		<a href="/blogs" class="font-normal text-xl text-white hover:text-2xl duration-300">Blogs</a>
 	</div>
 	<a href="https://github.com/arogyad/" target="_blank"
-		><i class="fa fa-github fa-2x hover:text-4xl text-white duration-200 md:ml-20" /></a
+		><i class="fa fa-github fa-2x hover:text-4xl text-white duration-200 ml-3" /></a
 	>
 	<a href="https://instagram.com/arogyad/" target="_blank"
 		><i class="fa fa-instagram fa-2x hover:text-4xl text-white duration-200 m-4" /></a
